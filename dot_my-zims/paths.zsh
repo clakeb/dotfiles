96fd263cefc38c35a186f31f7064c78df24784d2
Blake Barber <me@blakebarber.dev>
2021-08-10 00:24:31 -0500

    Add .chezmoiignore Add README.md Add dot_my-zims/init.zsh Add dot_my-zims/paths.zsh Add dot_my-zims/sources.zsh Update dot_profile Update dot_zimrc Rename run_a-update.sh.tmpl to run_after_a-update.sh.tmpl Rename run_once_a-init.sh.tmpl to run_once_before_a-init.sh.tmpl

---
+++ dot_my-zims/paths.zsh
@@ -0,0 +1,17 @@
+## /usr/local
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
+
+## Brew
+export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
+
+## PHP
+
+## Python
+
+## Ruby
+
+## Rust
+export PATH=$HOME/.cargo/bin:$PATH
+
+## HOME
+export PATH=$HOME/bin:$HOME/.bin:$HOME/.local/bin:$PATH
---
+++ dot_my-zims/sources.zsh
@@ -0,0 +1,5 @@
+## Starship
+eval "$(starship init zsh)"
+
+## iTerm2
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
