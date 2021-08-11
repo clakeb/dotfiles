7dd17c360ceb9452f3b74bef95ed0078c40cdd54
Blake Barber <me@blakebarber.dev>
2021-08-11 04:03:49 -0500

    Update dot_my-zims/paths.zsh

--- dot_my-zims/paths.zsh
+++ dot_my-zims/paths.zsh
@@ -13,5 +13,9 @@ export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
 ## Rust
 export PATH=$HOME/.cargo/bin:$PATH
 
+## Go
+export PATH=$HOME/go/bin:$PATH
+
 ## HOME
 export PATH=$HOME/bin:$HOME/.bin:$HOME/.local/bin:$PATH
+
