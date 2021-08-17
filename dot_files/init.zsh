5ca355e6e099b3c9155a0b61a8871af215c9b2c6
Blake Barber <me@blakebarber.dev>
2021-08-17 08:20:49 -0500

    install asdf

--- dot_files/init.zsh
+++ dot_files/init.zsh
@@ -2,4 +2,27 @@
 bindkey -v
 setopt CORRECT
 
+# source
+source $HOME/.files/paths.zsh
+source $HOME/.files/sources.zsh
+
+# Bashhub Server
+export BH_URL=http://localhost:8080
+
 # Aliases
+## Exa
+export EXA_COLORS='da=1;34:gm=1;34'
+
+alias ls='exa --group-directories-first'
+alias ll='ls -lhg --git --icons -s mod' # Long format, git status
+alias l='ll -a'                         # Long format, all files
+alias lr='l -T'                         # Long format, recursive as a tree
+alias lx='l -sextension'                # Long format, sort by extension
+alias lk='l -ssize'                     # Long format, largest file size last
+alias lt='l -smodified'                 # Long format, newest modification time last
+alias lc='l -schanged'                  # Long format, newest status change (ctime) last
+alias le='l --extended'                 # Long format, newest status change (ctime) last
+
+## ASDF globals
+### Python
+alias python='asdf exec python'
--- dot_files/sources.zsh
+++ dot_files/sources.zsh
@@ -1,9 +1,9 @@
-## Starship
-eval "$(starship init zsh)"
-
-## chruby
+## Chruby
 source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
 source /opt/homebrew/opt/chruby/share/chruby/auto.sh
 
 ## iTerm2
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+## Starship (last)
+eval "$(starship init zsh)"
