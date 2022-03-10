6cdb6492b2e912c5f5182fade2dca34699baf23e
Blake Barber <me@blakebarber.dev>
2022-03-09 20:54:24 -0600

    Update .chezmoiexternal.toml Update dot_files/paths.zsh Update run_once_before_001-install-packages.sh.tmpl

--- dot_files/paths.zsh
+++ dot_files/paths.zsh
@@ -1,5 +1,5 @@
 ## /usr/local
-# export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 
 ## Brew
 export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
@@ -15,6 +15,9 @@ export PATH=$HOME/.cargo/bin:$PATH
 
 ## Go
 export PATH=$HOME/go/bin:$PATH
+
+## Basher
+export PATH="$HOME/.basher/bin:$PATH"
 
 ## HOME
 export PATH=$HOME/bin:$HOME/.bin:$HOME/.local/bin:$PATH
